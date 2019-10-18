156dc3882e58f889f63aa02984888e0aead98bb7
pdalponte <pablodalponte@gmail.com>
2019-10-18 09:41:10 -0300

    Funcionando

---
+++ deployment_with_docker/run_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ver creacion de la red
+# se asume que ya esta creada
+
+docker container run \
+--rm \
+-d \
+-p 27017:27017 \
+-v datamongo:/data/db \
+--name db_server \
+--network mired \
+db_server
+
+docker container run \
+--rm \
+-d \
+-p 8000:8000 \
+-e DB_HOST=db_server \
+--name application_server \
+--network mired \
+application_server
+
+docker container run \
+--rm \
+-d \
+-p 8080:80 \
+--name web_server \
+--network mired \
+web_server
